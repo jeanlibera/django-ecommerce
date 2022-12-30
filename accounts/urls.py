f4bec08463c117465ca5498f578b4dec1497e38a
Jean Libera <jeanlibera@gmail.com>
2022-12-30 13:31:24 -0600

    Section 15 - Forgot Password

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard_page'),
     path('', views.dashboard),
 
-    path('activate/<uidb64>/<token>/', views.activate, name='activate_action')
+    path('activate/<uidb64>/<token>/', views.activate, name='activate_action'),
+    path('forgot-password/', views.forgot_password, name='forgot_password_page'),
+    path('reset-password-validate/<uidb64>/<token>/', views.reset_password_validate, name='reset_password_validate_action'),
+    path('reset-password/', views.reset_password, name='reset_password_page'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -106,11 +106,11 @@ def logout(request:HttpRequest):
     return redirect("login_page")
 
 def activate(request:HttpRequest, uidb64, token):
-    print(f"Starting logout request handler")
+    print(f"Starting activate request handler")
     print(f" uidb64={uidb64}, token={token}")
     try:
         # get the primary key of the user (uid = user id)
-        uid = urlsafe_base64_decode(uidb64)
+        uid = urlsafe_base64_decode(uidb64).decode()
         user = Account._default_manager.get(pk=uid)
     except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
         user = None
@@ -122,10 +122,88 @@ def activate(request:HttpRequest, uidb64, token):
         messages.success(request, 'Congratulations! Your account is activated.')
         return redirect('login_page')
     else:
-        messages.error('Invalid activation link')
+        messages.error(request, 'Invalid activation link')
         return redirect('register_page')
 
 @login_required(login_url = 'login_page')
 def dashboard(request: HttpRequest):
     return render(request, 'accounts/dashboard.html')
 
+def forgot_password(request: HttpRequest):
+    print("Starting forgot_password request handler")
+    if request.method == 'POST':
+        print("The method is POST")
+        email = request.POST['email']
+        if Account.objects.filter(email__iexact=email).exists():
+            user = Account.objects.get(email__iexact=email)
+            
+            # compose the reset password email
+            current_site = get_current_site(request)
+            mail_subject = 'Reset your password'
+            # the properties are objects that can be used in the html template, 
+            # accounts/account_verification_email.html
+            message = render_to_string('accounts/reset_password_email.html', {
+                'user': user,
+                'domain': current_site,
+                # we are encoding the user id with base64 encoding
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+		        'token': default_token_generator.make_token(user),
+            })
+            
+            send_email(subject=mail_subject, message=message, recipient_list=[email])
+            messages.success(request, "A password reset email has been sent to your email address")
+            return redirect('login_page')
+
+        else:
+            messages.error(request, "Account does not exist")
+            return redirect('forgot_password_page')
+
+    return render(request, 'accounts/forgot_password.html')
+
+def reset_password_validate(request:HttpRequest, uidb64, token):
+    ''' Validate the password link and redirect to reset_password_page '''
+    print(f"Starting reset_password_validate request handler")
+    print(f" uidb64={uidb64}, token={token}")
+    try:
+        # get the primary key of the user (uid = user id)
+        uid = urlsafe_base64_decode(uidb64).decode()
+        # returns the same result as Account.objects.get(id=uid)
+        user = Account._default_manager.get(pk=uid)
+        print("Account._default_manager.get(pk=uid) : found the user")
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+        print("Account._default_manager.get(pk=uid) : did not find the user ")
+
+    if (user is not None) and (default_token_generator.check_token(user, token)):
+        print(f"Found the user")
+        # save the uid inside the session
+        print(f"uid is {uid}, type is {type(uid)}")
+        request.session['uid'] = uid
+        print(f"Saved uid {uid} in the session")
+        messages.success(request, "Please reset your password")
+        return redirect('reset_password_page')
+    else:
+        messages.error(request, 'Invalid or expired reset password link')
+        return redirect('login_page')
+
+def reset_password(request: HttpRequest):
+    print(f"Starting reset_password request handler")
+    uid = request.session.get('uid')
+    print(f"The user id is {uid}")
+
+    if request.method == "POST":
+        password = request.POST["password"]
+        confirm_password = request.POST["confirm_password"]
+        if password == confirm_password:
+            user: Account = Account.objects.get(pk=uid)
+            user.set_password(password)
+            user.save()
+            print(f"The password was reset for uid {uid}")
+            messages.success(request, "The password was reset")
+            return redirect('login_page')
+        else:
+            messages.error(request, "Passwords do not match")
+            return redirect('reset_password_page')
+
+    else: # GET
+        return render(request, 'accounts/reset_password.html')
